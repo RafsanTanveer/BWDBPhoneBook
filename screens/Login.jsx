858b9e90fc1cde38f85f76abf1cf77aa229a0501
Rafsan Zani Rabbi <rzrt.edu@gmail.com>
2023-01-21 12:00:00 +0600

    Update Login.jsx

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -1,60 +1,87 @@
-import { View, Text, Image, Dimensions, ToastAndroid, ImageBackground, Keyboard, KeyboardAvoidingView, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { TextInput } from 'react-native-gesture-handler';
-import { Entypo } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import React, { useContext, useState } from 'react';
+import { Dimensions, Image, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native';
 
+const paniBhaban = '../assets/paniBhaban.png'
+const botom = '../assets/botom.png'
+const bwdbLogo = '../assets/bwdLogo.png'
 
 const height = Dimensions.get('window').height;
 const screenHeight = Dimensions.get('screen').height;
 const width = Dimensions.get('window').width;
 const screenWidth = Dimensions.get('screen').width;
-
-
 const Login = () => {
-
-   // const navigation = useNavigation();
-
-    const [id, setId] = useState("920219001")
-
-    const handleSubmit = () => {
+    const [pmisId, setpmisId] = useState("920219001")
+   
+    const handleSubmit = ({ pid }) => {
+        setId(pmisId)
         ToastAndroid.showWithGravity(id,
             ToastAndroid.SHORT,
             ToastAndroid.CENTER)
     }
-
-
     return (
-        <ImageBackground
-            style={{
-                flex: 1,
-                width: screenWidth * 1.009,
-                height: screenHeight,
-            }}
-            source={require('D:/ReactNative/nav-dr/assets/loginBackground2.png')} >
-
-            <View style={{ paddingTop: screenHeight * .572, width: screenWidth, alignItems: 'center', alignContent: 'center' }}>
-                <View style={{ flexDirection: 'row', paddingLeft: width * .15 }}>
-                    <TextInput selectionColor={'purple'} onChangeText={idText => setId(idText)}
-                        style={{ marginTop: 10, borderWidth: 1, borderColor: 'purple', height: height / 20, width: "70%", borderRadius: 10, marginBottom: 5 }} placeholder='   BWDB PMIS ID '>
-                        
-                        </TextInput>
-                    <TouchableOpacity onPress={() => handleSubmit()}>
-                        <View style={{ backgroundColor: 'purple', marginLeft: width * .029, justifyContent: 'center', marginTop: 10, alignItems: 'center', borderColor: 'purple', width: width * .13, height: height / 20, borderRadius: 10, marginBottom: 5 }}>
-                            <Entypo name="arrow-bold-right" size={24} color="white" />
-                        </View>
+        <View style={{ flex: 1, height: screenHeight, backgroundColor: 'white', flexDirection: 'column-reverse' }}>
+            <View style={{ height: screenHeight, }}>
+                <Image
+                    style={{
+                        resizeMode: 'stretch',
+                        position: 'absolute',
+                        width: screenWidth * 1,
+                        height: screenHeight * .6,
+                    }}
+                    source={require(paniBhaban)}
+                />
+                <Image
+                    style={{
+                        resizeMode: 'stretch',
+                        position: 'absolute',
+                        width: screenHeight * 0.12,
+                        height: screenHeight * 0.12,
+                        marginLeft: screenWidth * .72,
+                        marginTop: screenHeight * .42,
+                    }}
+                    source={require(bwdbLogo)}
+                />
+                <View style={{ marginTop: screenHeight * .6, alignItems: 'center', }}>
+                    <Text style={{ fontWeight: '900', fontSize: height * .04 }}>Phone Directory</Text>
+                    <Text style={{ fontWeight: '700', marginTop: 5, fontSize: height * .025 }}>Bangladesh Water Development Board</Text>
+                    <Text style={{ fontWeight: '700', marginTop: 5, fontSize: height * .022 }}>(BWDB)</Text>
+                    <TextInput
+                        maxLength={9}
+                        keyboardType={"decimal-pad"}
+                        selectionColor={'purple'}
+                        onChangeText={(txt) => setpmisId(txt)}
+                        style={{
+                            paddingLeft: 15,
+                            marginTop: 10,
+                            borderWidth: 1,
+                            borderColor: 'purple',
+                            height: height / 20,
+                            width: "70%",
+                            borderRadius: 10,
+                            marginBottom: 5,
+                            fontWeight: '700',
+                        }} placeholder='BWDB PMIS ID '>
+                    </TextInput>
+                    <TouchableOpacity onPress={() => (
+                        setId(pmisId)
+                        //handleSubmit(pmisId)
+                    )}
+                        style={{ height: height / 20, width: "70%", borderRadius: 10, alignItems: 'center', justifyContent: 'center', backgroundColor: '#FF0000' }}>
+                        <Text style={{ fontSize: screenHeight * .02, fontWeight: '700', color: 'white' }}>Signin</Text>
                     </TouchableOpacity>
                 </View>
-                <View style={{ alignItems: 'center', marginTop: 30 }}>
-                    <Text style={{ fontWeight: '900', fontSize: height * .04 }}>Phone Directory</Text>
-                    <Text style={{ fontWeight: '700', marginTop: 10, fontSize: height * .025 }}>Bangladesh Water Development Board</Text>
-                    <Text style={{ fontWeight: '700', marginTop: 10, fontSize: height * .022 }}>(BWDB)</Text>
+                <View style={{
+                    width: screenWidth, height: screenHeight * .1, position: 'absolute', //Here is the trick
+                    bottom: 0, //Here is the trick
+
+                }}>
+                    <View style={{ justifyContent: 'flex-end' }}>
+
+                        <Image style={{ width: screenWidth, height: screenHeight * .1, }} source={require(botom)} />
+                    </View>
                 </View>
             </View>
-
-
-        </ImageBackground >
-
+        </View>
     )
 }
 
