8de915b9b8c500ba7b5d52a2660487df5d0fd8ce
Rafsan Zani Rabbi <rzrt.edu@gmail.com>
2023-06-24 10:45:10 +0600

    Update DrawerContent.jsx

--- navigation/DrawerContent.jsx
+++ navigation/DrawerContent.jsx
@@ -3,7 +3,8 @@ import { View, Text, TouchableOpacity, Image, Dimensions } from "react-native";
 import { DrawerContentScrollView } from "@react-navigation/drawer";
 import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { imgSizeMini, txtSizeNormal, txtSizeBig } from "../utility/Scalling";
+import {useFonts} from 'expo-font'
 const { width, height } = Dimensions.get('window');
 
 import ExpendableDrawer from "./ExpendableDrawer";
@@ -15,6 +16,11 @@ const name = 'Arif Ikramul Azim';
 const designation = 'Addl. Director General'
 
 const DrawerContent = (props) => {
+
+
+    const [fontsLoaded] = useFonts({
+        'imperial-normal': require('../assets/fonts/imperial-normal.ttf'),
+    });
 
     const navigation = useNavigation();
     const { photo, officeAddres, presentOffice, name, logout, presentPost, presentCharge } = useContext(AuthContext);
@@ -32,26 +38,53 @@ const DrawerContent = (props) => {
                 paddingVertical: 15,
                 paddingHorizontal: 5
             }}>
-                <View style={{ flex: 2, height: width * .25, width: width * .25,  paddingTop:5 }}>
-                    <Image style={{ height: width * .22, width: width * .22, borderRadius:70 }} source={{ uri: "data:image/jpeg;base64," + photo }} />
+                <View style={{ flex: 2, height: width * .25, width: width * .25, paddingTop: 5 }}>
+                    <Image style={{ height: width * .22, width: width * .22, borderRadius: 70 }} source={{ uri: "data:image/jpeg;base64," + photo }} />
                 </View>
                 <View style={{ flex: 3.5 }}>
-                    <Text style={{ fontSize: width * .04, fontWeight:'600' }}>{name}</Text>
-                    <Text style={{ fontSize: width * .033, marginTop: 3 }}>{presentPost} {presentCharge === 'C' ?',CC':''}</Text>
+                    <Text style={{ fontSize: width * .04, fontWeight: '600' }}>{name}</Text>
+                    <Text style={{ fontSize: width * .033, marginTop: 3 }}>{presentPost} {presentCharge === 'C' ? ',CC' : ''}</Text>
                     <Text style={{ fontSize: width * .033, marginTop: 3 }}>{presentOffice}</Text>
                     <Text style={{ fontSize: width * .033, marginTop: 3 }}>{officeAddres}</Text>
                 </View>
             </View>
-            <DrawerContentScrollView style={{ backgroundColor: "#ffffff" }} {...props}>
+            <View style={{
+
+
+                left: width * .15,
+                justifyContent: 'center',
+                marginTop: height * .235,
+                // marginLeft:width*.15,
+                alignItems: 'center',
+                flexDirection: 'column',
+                backgroundColor: `${currentTheme}`,
+                borderRadius: height * .005,
+                marginHorizontal: 5,
+                marginVertical:5,
+                paddingVertical: 1,
+                paddingHorizontal: 10,
+                elevation:55,
+                position: 'absolute',
+                zIndex:120
+            }}>
+                <Text style={{
+                    color: 'white',
+                    height: height * (1 / 40),
+                    fontSize: txtSizeNormal,
+                    fontFamily: 'imperial-normal',
+                }}>RAFSAN ZANI RABBI</Text>
+            </View>
+
+            <DrawerContentScrollView style={{ backgroundColor: "#ffffff",marginTop:10 }} {...props}>
                 <ExpendableDrawer />
             </DrawerContentScrollView>
 
             <TouchableOpacity style={{ backgroundColor: '#ffffff' }} onPress={() => {
                 logout();
             }}>
-                <View style={{ margin: 20, backgroundColor: `${currentTheme}95`, height:30, width:80, borderRadius:10, alignItems:'center', justifyContent:'center' }}>
+                <View style={{ margin: 20, backgroundColor: `${currentTheme}95`, height: 30, width: 80, borderRadius: 10, alignItems: 'center', justifyContent: 'center' }}>
 
-                    <Text style={{fontSize:15, fontWeight:'600', color:'white'}}>Logout</Text>
+                    <Text style={{ fontSize: 15, fontWeight: '600', color: 'white' }}>Logout</Text>
 
                 </View>
             </TouchableOpacity>
